e8e40efcdb7afb6e53b410fbcf8f10682cb2a05a
carlosfiori44 <carlos.fiori@hotmail.com>
2024-08-18 16:47:35 -0300

    feat: add structure of the database init

---
+++ database/Dump20201026/loja_benedita_categoria.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
+--
+-- Host: localhost    Database: loja_benedita
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categoria` (
+  `id_categoria` smallint NOT NULL AUTO_INCREMENT,
+  `cor` varchar(16) DEFAULT NULL,
+  `formato` varchar(20) DEFAULT NULL,
+  `tipo_tecido` varchar(20) DEFAULT NULL,
+  `personalizado` mediumblob,
+  PRIMARY KEY (`id_categoria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categoria`
+--
+
+LOCK TABLES `categoria` WRITE;
+/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-10-26  0:14:50
---
+++ database/m5.sql
@@ -0,0 +1,75 @@
+use lojinha;
+drop database lojinha;
+create database lojinha
+
+default character set utf8
+default collate utf8_general_ci;
+use lojinha;
+show tables;
+select * from cliente;
+Insert into cliente(nome, email, senha) Values("admin", "admin@admin", "admin");
+
+Create table cliente(
+id_cliente smallint(4) primary key NOT NULL auto_increment,
+nome varchar(30) NOT NULL,
+cidade varchar(40) default 'Não especificado',
+sexo enum('M','F') NOT NULL,
+cep int(8) NULL,
+logradouro varchar(40) NULL,
+bairro varchar(40) NULL,
+email varchar(30) NOT NULL,
+senha varchar(16) NOT NULL
+) default charset = utf8;
+
+create table telefoneDoCliente(
+cliente_id smallint(4) not null,
+telefone_cliente int (10) NOT NULL
+)default charset = utf8;
+
+ALTER TABLE telefoneDoCliente ADD
+CONSTRAINT cliente_id FOREIGN KEY (cliente_id) REFERENCES cliente(id_cliente);
+
+create table compra(
+id_compra smallint(4) primary key not null auto_increment,
+data_venda date not null,
+data_entrega date not null,
+frete decimal(2,2) not null,
+total decimal(4,2) not null,
+quantidade_prod smallint(3) not null,
+id_produto smallint(4) not null,
+id_cliente smallint(4) not null
+)default charset = utf8;
+
+ALTER TABLE compra ADD
+CONSTRAINT id_cliente FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente);
+
+ALTER TABLE compra ADD
+CONSTRAINT id_produto FOREIGN KEY (id_produto) REFERENCES produto(id_produto);
+
+create table produto(
+id_produto smallint(4) primary key not null auto_increment,
+preco_custo decimal(4,2) null,
+preco_venda decimal(3,2) null,
+descricao varchar(65) null
+)default charset = utf8;
+
+create table categoria(
+id_categoria smallint(4) primary key not null auto_increment,
+cor varchar(16) null,
+formato varchar(20) null,
+tipo_tecido varchar(20) null,
+personalizado mediumblob null,
+prokey smallint(4) not null
+)default charset = utf8;
+
+ALTER TABLE categoria ADD
+CONSTRAINT prokey FOREIGN KEY (id_produto) REFERENCES produto(id_produto);
+
+create table fornecedor(
+id_fornecedor smallint(3) not null,
+nome_fornecedor varchar(40) not null,
+cnpj int(14) not null
+)default charset = utf8;
+
+ALTER TABLE produto ADD
+CONSTRAINT forkey FOREIGN KEY (id_fornecedor) REFERENCES fornecedor(id_fornecedor);
